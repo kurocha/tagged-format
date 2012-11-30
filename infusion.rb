98a27f268c1132670d2c70600c9de7ba3143c1b3
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-11-30 15:04:06 +1300

    Add unit-test dependency.

--- infusion.rb
+++ infusion.rb
@@ -7,6 +7,8 @@ required_version "0.2"
 require 'teapot/build'
 
 define_package "tagged-format-0.2.1" do |package|
+	package.depends = ['unit-test']
+	
 	package.build(:all) do |platform, environment|
 		top = Teapot::Build.top(Pathname.new(__FILE__).dirname)
 		
