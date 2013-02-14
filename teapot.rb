6ff73ddea1b29e3ddd5a8e23a0a232bf8a87c940
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-02-15 12:51:21 +1300

    Update to support teapot 0.6.

--- teapot.rb
+++ teapot.rb
@@ -3,11 +3,11 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.5"
+required_version "0.6"
 
 define_target "tagged-format" do |target|
 	target.install do |environment|
-		Teapot::Build.install_directory(package.path, 'source', environment)
+		install_directory(package.path, 'source', environment)
 	end
 	
 	target.depends :platform
@@ -20,7 +20,7 @@ end
 
 define_target "tagged-format-tests" do |target|
 	target.install do |environment|
-		Teapot::Build.install_directory(package.path, 'test', environment)
+		install_directory(package.path, 'test', environment)
 	end
 	
 	target.depends :platform
