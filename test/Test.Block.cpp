499b48b281f3556b86fa17eab5ffbf294538e456
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-02-18 23:39:29 +1300

    Improve the structure of the block unit test.

--- test/Test.Block.cpp
+++ test/Test.Block.cpp
@@ -2,22 +2,24 @@
 #include <UnitTest/UnitTest.h>
 #include <TaggedFormat/Block.h>
 
-UnitTest::Suite BlocksTestSuite {
-	"Test Blocks",
-	
-	{"Data type sizes and alignments",
-		[](UnitTest::Examiner & examiner) {
-			examiner.check_equal(sizeof(TaggedFormat::float32), 4);
-			examiner.check_equal(sizeof(TaggedFormat::float64), 8);
+namespace TaggedFormat {
+	UnitTest::Suite BlocksTestSuite {
+		"Test Blocks",
+		
+		{"Data type sizes and alignments",
+			[](UnitTest::Examiner & examiner) {
+				examiner.check_equal(sizeof(TaggedFormat::float32), 4);
+				examiner.check_equal(sizeof(TaggedFormat::float64), 8);
+			}
+		},
+		
+		{"Block header initialisation",
+			[](UnitTest::Examiner & examiner) {
+				TaggedFormat::Header header;
+				TaggedFormat::clear(header);
+				
+				examiner.check_equal(sizeof(TaggedFormat::Header), header.size);
+			}
 		}
-	},
-	
-	{"Block header initialisation",
-		[](UnitTest::Examiner & examiner) {
-			TaggedFormat::Header header;
-			TaggedFormat::clear(header);
-			
-			examiner.check_equal(sizeof(TaggedFormat::Header), header.size);
-		}
-	}
-};
+	};
+}
